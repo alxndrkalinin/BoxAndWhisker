c9c2120cb081f493ff2b7ac63b2b31842018121c
alxndrkalinin <alxndrkalinin@gmail.com>
2015-04-23 19:41:40 -0400

    fix(reformatResultTable): preserve original file indices in the first column

--- reformatResultTable.sh
+++ reformatResultTable.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-# first parameter - input file
-# second parameter - output file
-awk 'NR == 1 { gsub(/[A-Za-z0-9_\.\/-]*[0-9]+\.txt:[A-Za-z0-9_\.\/-]*/, "Case", $1); print}; NR%2==0 { gsub(/[A-Za-z0-9_\.\/-]*[0-9]+\.txt:[A-Za-z0-9_\.\/-]*/, NR/2, $1); print}' OFS="," "$1" > "$2"
+# first parameter - input file with LONI output table
+# second parameter - output file, preserving originl index
+while read -r line; do
+	if [[ "$line" =~ .*File ]];
+		then
+			line="${line##*:}";
+		else
+			line="${line##*-}";
+			index="${line%%.*}";
+			line="${line#*[[:space:]]}";
+			line="$index $line"
+	fi;
+	echo $line;
+done < "$1" >> "$2"
